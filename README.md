c7d765a62217d683536dfa3364d59dfd5cfc8e69
Eddie Jesinsky <ejesinsky@invoca.com>
2024-03-30 12:24:02 -0600

    Dijkstra implemented

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # cs6045-proj
 
+Shortest path implementations from source to other nodes (not providing previous hop nodes)- Bellman Ford, Floyd Warshall (from source), Dijkstra
+
 Python 3.8.18, recommended with pyenv
 
 Install packages:
@@ -9,3 +11,14 @@ pip install -r requirements.txt
 Run tests:
 
 python setup.py pytest
+
+Graph format:
+
+{
+  "edges": {
+  "A->B": 3,
+  "B->C": 2,
+  "A->C": 5
+  },
+  "vertices": ["A", "B", "C"]
+}
--- shortestpaths/__init__.py
+++ shortestpaths/__init__.py
@@ -1,3 +1,4 @@
+import math
 import os
 import psutil
 import time
@@ -9,12 +10,12 @@ def bellmanford(graph, source, profile=False):
         return result, prof
     return _bellmanford(graph, source)
 
-def astar(graph, source, profile=False):
+def floydwarshall(graph, source, profile=False):
     if profile == True:
-        result, prof = _astar(graph, source, monitor=proc())
+        result, prof = _floydwarshall(graph, source, monitor=proc())
         write_graphs(prof)
         return result, prof
-    return _astar(graph, source)
+    return _floydwarshall(graph, source)
 
 def dijkstra(graph, source, profile=False):
     if profile == True:
@@ -26,23 +27,80 @@ def dijkstra(graph, source, profile=False):
 def _bellmanford(graph, source, monitor=None):
     prof = []
     if monitor:
-        prof.append(tick(monitor))
-        return None, prof
-    return None
+        tic = lambda: prof.append(tick(monitor))
+    else:
+        tic = lambda: None
 
-def _astar(graph, source, monitor=None):
+    if monitor:
+        return None, prof
+    else:
+        return None
+
+# floyd warshall calculates all pairs, but we only care about them from source for consistency
+# so we need to use path reconstruction at the end for each vertice from the source for calculation
+def _floydwarshall(graph, source, monitor=None):
     prof = []
     if monitor:
-        prof.append(tick(monitor))
+        tic = lambda: prof.append(tick(monitor))
+    else:
+        tic = lambda: None
+
+    if monitor:
         return None, prof
-    return None
+    else:
+        return None
 
 def _dijkstra(graph, source, monitor=None):
     prof = []
     if monitor:
-        prof.append(tick(monitor))
-        return None, prof
-    return None
+        tic = lambda: prof.append(tick(monitor))
+    else:
+        tic = lambda: None
+
+    tic()
+    dist = {}
+    prev = {}
+    Q = []
+    for v in graph["vertices"]:
+        dist[v] = math.inf
+        prev[v] = None
+        Q.append(v)
+        tic()
+    dist[source] = 0
+
+    while len(Q) > 0:
+        u = min_dist(Q, dist)
+        Q.remove(u)
+        tic()
+        for v in neighbors(Q, u, graph):
+            alt = dist[u] + edge(u, v, graph)
+            if alt < dist[v]:
+                dist[v] = alt
+                prev[v] = u
+            tic()
+    tic()
+    if monitor:
+        return dist, prof
+    else:
+        return dist
+
+def edge(u, v, graph):
+    length = graph["edges"][f"{u}->{v}"]
+    if length != None:
+        return length
+    else:
+        return math.inf
+
+def min_dist(Q, dist):
+    filtered_dist = {key: dist[key] for key in Q}
+    return min(filtered_dist, key=filtered_dist.get)
+
+def neighbors(Q, u, graph):
+    neighbors = []
+    for v in Q:
+        if graph["edges"][f"{u}->{v}"]:
+            neighbors.append(v)
+    return neighbors
 
 def tick(monitor):
     return {
