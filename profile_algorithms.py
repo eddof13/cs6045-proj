acbf7290350eb8c22ba3c21eccf9497ba572f8b0
Eddie Jesinsky <ejesinsky@invoca.com>
2024-04-29 18:02:49 -0600

    Profile on test files

--- profile_algorithms.py
+++ profile_algorithms.py
@@ -3,6 +3,7 @@ import psutil
 import time
 import shortestpaths
 import multiprocessing
+import random
 from cProfile import Profile
 from pstats import SortKey, Stats
 from threading import Event
@@ -27,45 +28,56 @@ def init_ticker(q, f, pid):
     schedule(ticker, interval=0.1)
     run_loop(stop_event=stop_event)
     
-def load_func(q, f, func, graph, source):
+def load_func(q, f, func, graph, source, name):
     pid = os.getpid()
     p = multiprocessing.Process(target=init_ticker, args=(q, f, pid))
     p.start()
     with Profile() as profile:
-        print(func(graph, source))
-        Stats(profile).strip_dirs().sort_stats(SortKey.CALLS).print_stats()
+        results = str(func(graph, source))
+        Stats(profile).strip_dirs().sort_stats(SortKey.CALLS).dump_stats(name.split(".")[0] + " HEATMAP.txt")
+    with open(name.split(".")[0] + " RESULTS.txt", "w") as output:
+        output.write(results)
     f.put("die")
 
-def profile(func, graph, source):
+def profile(func, graph, source, name):
     q = multiprocessing.Queue()
     f = multiprocessing.Queue()
-    p = multiprocessing.Process(target=load_func, args=(q, f, func, graph, source))
+    p = multiprocessing.Process(target=load_func, args=(q, f, func, graph, source, name))
     p.start()
     p.join()
     while not q.empty():
-        print(q.get())
+        result = str(q.get())
+    with open(name.split(".")[0] + " PROFILE.txt", "a") as output:
+        output.write(result)
+
+def test_files():
+    root_folder = "graph_generation"
+    extensions = [".txt", ".csv", ".test"]
+    test_files = []
+    for subdir, dirs, files in os.walk(root_folder):
+        for file in files:
+            ext = os.path.splitext(file)[-1].lower()
+            if ext in extensions:
+                test_files.append(os.path.join(subdir, file))
+    return test_files
 
 if __name__ == "__main__":
-    graph = {
-        "edges": {
-            "A->B": 3,
-            "B->C": 2,
-            "A->C": 5
-        },
-        "vertices": ["A", "B", "C"]
-    }
-    source = "A"
+    for test_file in test_files():
+        with open(test_file, "r") as file:
+            data = file.read()
+            graph = eval("{ " + data + " }") # this is quite dangerous, but we are only using it for profiling
+            source = random.choice(graph["vertices"]) # picking a source at random
 
-    print("Starting dijkstra 1")
-    profile(shortestpaths.dijkstra, graph, source)
-    print("Finishing dijkstra 1")
+            print(f"Starting dijkstra #{test_file} with source #{source}")
+            profile(shortestpaths.dijkstra, graph, source, test_file + " dijkstra")
+            print(f"Finishing dijkstra #{test_file} with source #{source}")
 
-    print("Starting bellmanford 1")
-    profile(shortestpaths.bellmanford, graph, source)
-    print("Finishing bellmanford 1")
+            print(f"Starting bellmanford #{test_file} with source #{source}")
+            profile(shortestpaths.bellmanford, graph, source, test_file + " bellmanford")
+            print(f"Finishing bellmanford #{test_file} with source #{source}")
 
-    print("Starting floydwarshall 1")
-    profile(shortestpaths.floydwarshall, graph, source)
-    print("Finishing floydwarshall 1")
+            print(f"Starting floydwarshall #{test_file} with source #{source}")
+            profile(shortestpaths.floydwarshall, graph, source, test_file + " floydwarshall")
+            print(f"Finishing floydwarshall #{test_file} with source #{source}")
 
 
